c2dc70c27bc7a4b43ea0f0382a25051d339a1026
unknown <el_word9@yahoo.com>
2024-02-12 18:14:06 -0600

    Added API Key and URL

--- Main/script.js
+++ Main/script.js
@@ -1,7 +1,7 @@
 // Global variables
 var searchHistory = [];
 var weatherApiRootUrl = 'https://api.openweathermap.org';
-var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+var weatherApiKey = '08c44e3b9b7801a7054a82a1a4977e35';
 
 // DOM element references
 var searchForm = document.querySelector('#search-form');
